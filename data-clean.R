a85d12b0ef9861a27278dcce43035aafcc0eaf65
mooromets <sergey.sambor@gmail.com>
2017-09-21 19:15:58 +0200

    cleaining cities' data

---
+++ data-clean.R
@@ -0,0 +1,31 @@
+# Clean Cities
+csvInput <- "../worldcitiespop.txt"
+gzInput <- paste0(csvInput, ".gz")
+csvOutput <- "data/cities.csv"
+gzOutput <- paste0(csvOutput, ".gz")
+uefa_codes <- tolower(c("AL", "AD", "AM", "AT", "BY", "BE", "BA", "BG", "CH", 
+            "CY", "CZ", "DE", "DK", "EE", "ES", "FO", "FI", "FR", "GB", "GE", 
+            "GI", "GR", "HU", "HR", "IE", "IS", "IT", "LT", "LU", "LV", 
+            "MC", "MK", "MT", "NO", "NL", "PO", "PT", "RO", "RU", "SE", 
+            "SI", "SK", "SM", "TR", "UA", "VA", "AZ", "KZ", "IL"))
+
+if (!file.exists(gzOutput)) {
+  require(R.utils)
+  if (!file.exists(csvOutput)) {
+    if (!file.exists(csvInput))
+      R.utils::gunzip(filename = gzInput, remove = FALSE)
+    print("Reading full cities' data...")
+    cities <- read.csv(csvInput)
+    print("Removing NA-s...")
+    clean <- na.omit(cities)
+    require(dplyr)
+    clean <- filter(clean, !(Country %in% uefa_codes))
+    print("Saving data...")
+    write.csv(clean, csvOutput)
+    rm(clean)
+    rm(cities)
+    file.remove(csvInput)
+  }
+  print("Compressing data...")
+  R.utils::gzip(csvOutput)
+}
